4d9bfbf5bbe6b196f26ebf756d0b44306bd7d156
qiuzhe <2810201146@qq.com>
2022-04-06 18:00:58 +0800

    设置baseUrl

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: 邹洋
+ * @Date: 2022-04-06 16:42:25
+ * @Email: 2810201146@qq.com
+ * @LastEditors:  
+ * @LastEditTime: 2022-04-06 17:59:27
+ * @Description: 
+ */
+module.exports = {
+    // 基本路径
+    baseUrl: './',
+    // 输出文件目录
+    outputDir: 'dist',
+    // webpack-dev-server 相关配置
+    devServer: {
+      port: 8888,
+    },
+  }
